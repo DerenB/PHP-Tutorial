e8b11b1ea4dfe4e35e396a6c453fc8de04c64d1c
Deren B <47374311+DerenB@users.noreply.github.com>
2023-03-28 19:44:01 -0400

    Video 25
    Connect to DataBase

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+    // Connect to DataBase
+    $connection = mysqli_connect('localhost', 'deren', '1234', 'ninja_pizza');
+
+    // Test Connection
+    // If unsuccessful, will resolve as true due to '!'
+    if(!$connection) {
+        echo 'Connection Error: ' . mysqli_connect_error();
+    }
+
 ?>
 
 <!DOCTYPE html>
