d345dfe7e288926b01e87491b0d89fd5544b73ed
Deren B <47374311+DerenB@users.noreply.github.com>
2023-03-28 21:57:30 -0400

    Video 32: Deletion
    Delete value from database

--- details.php
+++ details.php
@@ -3,6 +3,24 @@
     // Database Connection
     include('config/db_connect.php');
 
+    // Delete method
+    if(isset($_POST['delete'])) {
+        // Gets ID to be deleted
+        $id_to_delete = mysqli_real_escape_string($connection, $_POST['id_to_delete']);
+
+        // SQL string for deletion
+        $sqlDelete = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+        // Execute Query
+        if(mysqli_query($connection, $sqlDelete)) {
+            // success
+            header('Location: index.php');
+        } else {
+            // failure
+            echo 'Query Error: ' . mysqli_error($connection);
+        }
+    }
+
     // Check GET request id parameter
     if(isset($_GET['id'])) {
         $id = mysqli_real_escape_string($connection, $_GET['id']);
@@ -38,6 +56,12 @@
             <p><?php echo date($pizza['created_at']); ?></p>
             <h5>Ingredients:</h5>
             <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+            <!-- Form to Delete Pizza -->
+            <form action="details.php" method="POST">
+                <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+                <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+            </form>
 
         <?php else: ?>
 
